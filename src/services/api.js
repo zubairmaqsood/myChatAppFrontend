6f8614ac8db18f22eb78549b9000f32aafeeff88
zubairmaqsood <149279927+zubairmaqsood@users.noreply.github.com>
2026-03-20 00:31:58 +0500

    added .env file

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:3000",
-   
+  baseURL:import.meta.env.VITE_BACKEND_URL 
 })
 
 // This interceptor will run eveytime request is sent
